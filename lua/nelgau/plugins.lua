f02a765479f39022938fc2fcea57e96cd77ea9ea
Nelson Gauthier <nelson.gauthier@gmail.com>
2022-11-12 23:21:52 -0800

    packer: Limit concurrency and enable autoremove

--- lua/nelgau/plugins.lua
+++ lua/nelgau/plugins.lua
@@ -5,114 +5,120 @@ vim.cmd([[
   augroup end
 ]])
 
-return require('packer').startup(function(use)
-  use("wbthomason/packer.nvim")
+return require('packer').startup({
+  function(use)
+    use("wbthomason/packer.nvim")
 
-  --
-  -- Colorschemes
-  --
+    --
+    -- Colorschemes
+    --
 
-  use({
-    "shaunsingh/nord.nvim",
-    requires = "folke/lsp-colors.nvim",
-    config = function()
-      require("nelgau.color.nord").setup()
-    end,
-  })
+    use({
+      "shaunsingh/nord.nvim",
+      requires = "folke/lsp-colors.nvim",
+      config = function()
+        require("nelgau.color.nord").setup()
+      end,
+    })
 
-  use({
-    "gruvbox-community/gruvbox",
-    requires = "folke/lsp-colors.nvim",
-    config = function()
-      require("nelgau.color.gruvbox").setup()
-    end,
-  })
+    use({
+      "gruvbox-community/gruvbox",
+      requires = "folke/lsp-colors.nvim",
+      config = function()
+        require("nelgau.color.gruvbox").setup()
+      end,
+    })
 
-  --
-  -- Statusline
-  --
+    --
+    -- Statusline
+    --
 
-  use({
-    "nvim-lualine/lualine.nvim",
-    config = function()
-      require("nelgau.config.lualine").setup()
-    end,
-  })
+    use({
+      "nvim-lualine/lualine.nvim",
+      config = function()
+        require("nelgau.config.lualine").setup()
+      end,
+    })
 
-  --
-  -- Language Server
-  --
+    --
+    -- Language Server
+    --
 
-  use({
-    "neovim/nvim-lspconfig",
-    requires = {
-      -- "williamboman/nvim-lsp-installer",
-      "ray-x/lsp_signature.nvim",
-    },
-    config = function()
-      require("nelgau.config.lspconfig").setup()
-    end,
-  })
-
-  use({
-    "hrsh7th/nvim-cmp",
-    requires = {
-      "nvim-treesitter",
+    use({
       "neovim/nvim-lspconfig",
-      "hrsh7th/cmp-nvim-lsp",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-nvim-lua",
-      "hrsh7th/cmp-cmdline",
-    },
-    config = function()
-      require("nelgau.config.cmp").setup()
-    end,
-  })
+      requires = {
+        -- "williamboman/nvim-lsp-installer",
+        "ray-x/lsp_signature.nvim",
+      },
+      config = function()
+        require("nelgau.config.lspconfig").setup()
+      end,
+    })
 
-  --
-  -- Syntax
-  --
+    use({
+      "hrsh7th/nvim-cmp",
+      requires = {
+        "nvim-treesitter",
+        "neovim/nvim-lspconfig",
+        "hrsh7th/cmp-nvim-lsp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-nvim-lua",
+        "hrsh7th/cmp-cmdline",
+      },
+      config = function()
+        require("nelgau.config.cmp").setup()
+      end,
+    })
 
-  use({
-    "nvim-treesitter/nvim-treesitter",
-    requires = {
-      "nvim-treesitter/nvim-treesitter-context",
-      "nvim-treesitter/playground",
-    },
-    run = ":TSUpdate",
-    config = function()
-      require("nelgau.config.treesitter").setup()
-    end,
-  })
+    --
+    -- Syntax
+    --
 
-  --
-  -- Navigation and Editing
-  --
+    use({
+      "nvim-treesitter/nvim-treesitter",
+      requires = {
+        "nvim-treesitter/nvim-treesitter-context",
+        "nvim-treesitter/playground",
+      },
+      run = ":TSUpdate",
+      config = function()
+        require("nelgau.config.treesitter").setup()
+      end,
+    })
 
-  use({
-    -- use "preservim/nerdtree"
-    "santiagovrancovich/nerdtree",
-    branch = "hotfix-issue-#1321",
-    config = function()
-      require("nelgau.config.nerdtree").setup()
-    end,
-  })
+    --
+    -- Navigation and Editing
+    --
 
-  use({
-    "gpanders/editorconfig.nvim",
-  })
+    use({
+      -- use "preservim/nerdtree"
+      "santiagovrancovich/nerdtree",
+      branch = "hotfix-issue-#1321",
+      config = function()
+        require("nelgau.config.nerdtree").setup()
+      end,
+    })
 
-  --
-  -- Input
-  --
+    use({
+      "gpanders/editorconfig.nvim",
+    })
 
-  use({
-    "b0o/mapx.nvim",
-    config = function()
-      require("nelgau.config.mappings").setup()
-    end,
-  })
-end)
+    --
+    -- Input
+    --
+
+    use({
+      "b0o/mapx.nvim",
+      config = function()
+        require("nelgau.config.mappings").setup()
+      end,
+    })
+  end,
+  config = {
+    max_jobs = 50,
+    autoremove = true,
+  },
+})
