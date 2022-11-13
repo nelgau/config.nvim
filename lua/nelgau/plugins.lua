2d726890fdc8e2d23e35bfa53375b6f004de9757
Nelson Gauthier <nelson.gauthier@gmail.com>
2022-11-12 16:48:31 -0800

    Install editorconfig.nvim

--- lua/nelgau/plugins.lua
+++ lua/nelgau/plugins.lua
@@ -1,16 +1,25 @@
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 return require('packer').startup(function(use)
-    use('wbthomason/packer.nvim')
+  use('wbthomason/packer.nvim')
 
-    use('nvim-lualine/lualine.nvim')
+  use('nvim-lualine/lualine.nvim')
 
-    use('neovim/nvim-lspconfig')
+  use('neovim/nvim-lspconfig')
 
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-    use('nvim-treesitter/nvim-treesitter-context')
-    use('nvim-treesitter/playground')
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/nvim-treesitter-context')
+  use('nvim-treesitter/playground')
 
-    use('gruvbox-community/gruvbox')
+  use('gruvbox-community/gruvbox')
 
-    -- use "preservim/nerdtree"
-    use {'santiagovrancovich/nerdtree', branch = 'hotfix-issue-#1321'}
+  use('gpanders/editorconfig.nvim')
+
+  -- use "preservim/nerdtree"
+  use {'santiagovrancovich/nerdtree', branch = 'hotfix-issue-#1321'}
 end)
