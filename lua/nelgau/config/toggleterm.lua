995ee35068e2fab48dda517262a5b450435de360
Nelson Gauthier <nelson.gauthier@gmail.com>
2022-11-13 12:04:27 -0800

    toggleterm: Open login shell using environment zsh

--- lua/nelgau/config/toggleterm.lua
+++ lua/nelgau/config/toggleterm.lua
@@ -1,9 +1,12 @@
-local M = {}
+local M = {
+  terminals = {},
+}
 
 function M.setup()
-  require("toggleterm").setup({
+  local toggleterm = require("toggleterm")
+
+  toggleterm.setup({
     size = 40,
-    open_mapping = [[<c-\>]],
     hide_numbers = true,
     shade_terminals = true,
     shading_factor = 2,
@@ -19,6 +22,54 @@ function M.setup()
       },
     },
   })
+
+  M.define_terminal("main", [[<c-\>]], M.default_shell_cmd())
+end
+
+function M.define_terminal(name, mapping, cmd)
+  local Terminal = require("toggleterm.terminal").Terminal
+
+  function build_command(method)
+    return "<cmd>lua __toggleterm__" .. method .. "('" .. name .. "')<CR>"
+  end
+
+  local keymap = vim.api.nvim_set_keymap
+  local buf_keymap = vim.api.nvim_buf_set_keymap
+  local kopt = {noremap = true, silent = true}
+
+  M.terminals[name] = Terminal:new({
+    cmd = cmd,
+    clear_env = true,
+    on_open = function(term)
+      buf_keymap(term.bufnr, "t", mapping, build_command("toggle"), kopt)
+      buf_keymap(term.bufnr, "t", "<c-l>", build_command("clear"), kopt)
+    end,
+  })
+
+  keymap("n", mapping, build_command("toggle"), kopt)
+end
+
+function M.default_shell_cmd()
+  -- From http://lua-users.org/wiki/CommonFunctions
+  -- TODO: Seems unnecessary. Is there a better way to do this?
+  function rtrim(s)
+    local n = #s
+    while n > 0 and s:find("^%s", n) do n = n - 1 end
+    return s:sub(1, n)
+  end
+
+  local zsh_path = rtrim(vim.fn.system("which zsh"))
+  local zsh_cmd = zsh_path .. " --login"
+
+  return zsh_cmd
+end
+
+function __toggleterm__toggle(name)
+  M.terminals[name]:toggle()
+end
+
+function __toggleterm__clear(name)
+  M.terminals[name]:clear()
 end
 
 return M
