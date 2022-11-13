51256fe6973900603a19a933be5c6dea4941f7b8
Nelson Gauthier <nelson.gauthier@gmail.com>
2022-11-13 01:22:22 -0800

    Disable distracting icons in nvim-tree

--- lua/nelgau/config/nvim-tree.lua
+++ lua/nelgau/config/nvim-tree.lua
@@ -2,6 +2,16 @@ local M = {}
 
 function M.setup()
   require("nvim-tree").setup({
+    renderer = {
+      icons = {
+        show = {
+          file = false,
+          folder = false,
+          folder_arrow = true,
+          git = true,
+        },
+      },
+    },
     filters = {
       dotfiles = false,
     },
