1c9de924bd4f744cb636c081140fb8ee2247d37d
Nelson Gauthier <nelson.gauthier@gmail.com>
2022-12-24 05:27:56 -0500

    treesitter: Autoinstall only a few languages

--- lua/nelgau/config/treesitter.lua
+++ lua/nelgau/config/treesitter.lua
@@ -4,8 +4,16 @@ function M.setup()
   require("nvim-treesitter.install").prefer_git = false
 
   require("nvim-treesitter.configs").setup({
-    ensure_installed = "all",
-    sync_install = true,
+    ensure_installed = {
+      "c",
+      "lua",
+      "python",
+      "ruby",
+      "rust",
+      "vim",
+    },
+    sync_install = false,
+    auto_install = true,
     highlight = {
       enable = true,
       additional_vim_regex_highlighting = false,
